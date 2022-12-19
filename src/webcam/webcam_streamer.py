98484a11eb2a585ffd84c530e9d5ce4c6576f3d7
Tian Sizhe <yztszh168@163.com>
2022-12-19 16:21:23 +0100

    add comments

--- src/webcam/webcam_streamer.py
+++ src/webcam/webcam_streamer.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+"""WebCamStreamer Object, streamming images from an external webcam"""
 import time
 from threading import Event, Thread
 
@@ -8,17 +9,30 @@ from v4l2py import Device
 
 
 class webCamStreamer:
-    def __init__(self, id) -> None: 
-        self.cam = Device.from_id(id)
-        self.cam.video_capture.set_format(1920, 1080, 'MJPG')
+    """Class to interact with external webcam.
+    
+    A thread is running on background to retrieve the most recent image. 
+    To get the most recent frame, call the function get_frame()
+    
+    """
+    def __init__(self, id:int) -> None: 
+        """Initializer
 
+        Args:
+            id (int): camera id. 0 if no other camera is avaiable. 
+                      could be other value if there are integrated cameras
+        """
+        self.id = id
+        self.cam = Device.from_id(self.id)
+        # Set format of the streamming
+        self.cam.video_capture.set_format(1920, 1080, 'MJPG')
         # Start frame retrieval thread
         self.thread = Thread(target=self.update, args=())
         self.thread.daemon = True
-        
+        # Event to control the thread
         self.webcam_stop = Event()
         self.webcam_stop.clear()
-
+        # start streamming
         self.thread.start()
         
     def update(self):
