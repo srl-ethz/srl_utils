8de25a6fc06bd4d9f1ec6c67941f58e381b5e334
Tian Sizhe <yztszh168@163.com>
2022-12-20 14:50:57 +0100

    a short script to use streamer and detector

---
+++ src/webcam/webcam_detection.py
@@ -0,0 +1,59 @@
+import subprocess
+import time
+
+import cv2
+import numpy as np
+import rospy
+from cv_bridge import CvBridge
+from detector import Detector
+from sensor_msgs.msg import CompressedImage, Image
+from v4l2py import Device
+from webcam_streamer import (disable_auto_focus, enable_auto_focus, set_focus,
+                             webCamStreamer)
+
+
+def main():
+    camera_id = 4
+    enable_auto_focus(camera_id)
+    disable_auto_focus(camera_id)
+    set_focus(camera_id, 60)
+    
+    detector = Detector()
+    streamer = webCamStreamer(camera_id)
+
+    # Parameters for detection
+    detector.set_radii_range(12, 20)
+    detector.set_param2_kpt(30)  
+    detector.set_min_dist(25)
+    detector.set_hough_line_threshold(6)
+    num_marker = 10
+    # wait one second for webcam to start
+    time.sleep(1)
+
+    # image_update_rate = rospy.Rate(30)
+
+    try:
+        while not rospy.is_shutdown(): 
+            # t1 = time.time()
+            image = streamer.get_frame()
+            detector.set_image(image)
+            img, length  = detector.detect_kpt(num_marker)
+            # If all the markers are detected, get center coordinates
+            if length == num_marker:  
+                center_x_list = detector.get_center_x_list()
+                center_y_list = detector.get_center_y_list()
+            # t2 = time.time()
+            # print(f"detection loop {t2-t1}")
+            # image_update_rate.sleep()
+            
+            # Display detection results
+            cv2.namedWindow("webcam", cv2.WINDOW_NORMAL)
+            cv2.imshow("webcam", img)
+            cv2.waitKey(3)    
+    finally:
+        streamer.stop()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()  
+
