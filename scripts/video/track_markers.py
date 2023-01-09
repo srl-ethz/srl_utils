0e9307b25b455fe70bcc692e5e35c2dc15d70cc4
Mike Yan Michelis <46975745+mmichelis@users.noreply.github.com>
2023-01-09 10:39:30 +0100

    Add more documentation

--- scripts/video/track_markers.py
+++ scripts/video/track_markers.py
@@ -2,11 +2,11 @@
 
 """Track bounding boxes within video.
 
-Usage: python3 track_markers.py -f <filepath> -n <N_bounding_boxes> -s <start_frame> -e <end_frame>
+Usage: python3 track_markers.py -f <filepath> -n <num_boxes> -s <start_frame> -e <end_frame>
 
 Example: python3 track_markers.py -f ~/Downloads/tracking/vid.mp4 -n 2 -s 10 -e 50
 
-Script for tracking N manually chosen bounding boxes within video. Point this script to the video file you would like to track and choose how many N bounding boxes are desired. These bounding box centers (markers) are stored in a CSV file afterwards. Two videos are created as well, one is the original video cut to [start_frame, end_frame], and the other is with the tracking bounding boxes displayed.
+Script for tracking N manually chosen bounding boxes within video. Point this script to the video file you would like to track and choose how many N bounding boxes are desired. These bounding box centers (markers) are stored in a CSV file afterwards. Two videos are created as well, one is the original video cut to [start_frame, end_frame], and the other is with the tracking bounding boxes displayed. If you for some reason desire to quite the tracking earlier than end_frame, you can press q to exit out.
 
 Note: OpenCV installation can sometimes have trouble with cv2.legacy.MultiTracker_create(), the version that worked is:
 opencv-contrib-python 4.5.2.52
@@ -28,7 +28,7 @@ import numpy as np
     required=True,
 )
 @click.option(
-    "--n_bounding_boxes", "-n", default=2, help="Number of bounding boxes."
+    "--num_boxes", "-n", default=2, help="Number of bounding boxes."
 )
 @click.option(
     "--start_frame", "-s", default=0, help="Relevant starting frame of video."
@@ -37,14 +37,14 @@ import numpy as np
     "--end_frame", "-e", default=100, help="Relevant ending frame of video."
 )
 def track_markers(
-    filepath: str, n_bounding_boxes: int, start_frame: int, end_frame: int
+    filepath: str, num_boxes: int, start_frame: int, end_frame: int
 ):
     """Track bounding boxes within video.
     Script for tracking N manually chosen bounding boxes within video. Point this script to the video file you would like to track and choose how many N bounding boxes are desired. These bounding box centers (markers) are stored in a CSV file afterwards. Two videos are created as well, one is the original video cut to [start_frame, end_frame], and the other is with the tracking bounding boxes displayed
 
     Args:
         filepath (str): Video filepath.
-        n_bounding_boxes (int): Number of bounding boxes.
+        num_boxes (int): Number of bounding boxes.
         start_frame (int): Starting frame of video at which tracking should start.
         end_frame (int): Last frame of video that should be considered (inclusive) during tracking.
     """
@@ -61,7 +61,7 @@ def track_markers(
 
         # Find bounding boxes
         bboxes = []
-        for i in range(n_bounding_boxes):
+        for i in range(num_boxes):
             bboxes.append(cv2.selectROI(f"Select {i+1}-th Marker", frame))
         break
     else:
@@ -71,7 +71,7 @@ def track_markers(
 
     # Create multiple trackers
     trackers = cv2.legacy.MultiTracker_create()
-    for i in range(n_bounding_boxes):
+    for i in range(num_boxes):
         trackers.add(cv2.legacy.TrackerCSRT_create(), frame, bboxes[i])
 
     # Tracking
@@ -111,22 +111,22 @@ def track_markers(
         if not found:
             cv2.putText(
                 frame,
-                "One of the objects not found",
-                (20, 70),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.75,
-                (0, 0, 255),
-                2,
+                text="One of the objects not found",
+                org=(20, 70),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.75,
+                color=(0, 0, 255),
+                thickness=2,
             )
 
         for box in bboxes_new:
             topleft = (int(box[0]), int(box[1]))
             botright = (int(box[0] + box[2]), int(box[1] + box[3]))
-            cv2.rectangle(frame, topleft, botright, (0, 0, 255), 2)
+            cv2.rectangle(frame, topleft, botright, color=(0, 0, 255), thickness=2)
 
         curr_markers = []
         # Compute relevant values of motion marker positions
-        for i in range(n_bounding_boxes):
+        for i in range(num_boxes):
             # Compute centers of found bounding boxes
             curr_center = np.array(
                 [
@@ -139,10 +139,11 @@ def track_markers(
         # Markers are stored as a flattened array, x then y coordinate of each point.
         markers.append(np.array(curr_markers).flatten())
 
-        cv2.imshow("Tracker", frame)
+        cv2.imshow("Tracker (press Q to exit early)", frame)
         tracked_movie.write(frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
+            print("Exited early!")
             break
 
     cut_movie.release()
