b73f111b6b1f7fa131a93e068703228c699a0dbe
Tian Sizhe <yztszh168@163.com>
2023-02-28 17:02:43 +0100

    reformat

--- src/ft_sensor/include/ftconverter.h
+++ src/ft_sensor/include/ftconverter.h
@@ -34,14 +34,14 @@
 #ifndef FTCONVERTER_H
 #define FTCONVERTER_H
 
-#include <stdio.h>
-#include <string>
+#include "ftconfig.h"
 #include "ros/ros.h"
 #include "std_msgs/Float32MultiArray.h"
-#include "ftconfig.h"
+#include <stdio.h>
+#include <string>
 
 class FTConverter {
- public:
+public:
   explicit FTConverter(char *calfilepath);
   ~FTConverter();
 
@@ -73,21 +73,20 @@ class FTConverter {
    */
   void adcCallback(std_msgs::Float32MultiArray msg);
 
- private:
+private:
   // std::string calfilepath_;  // path to the calibration files
-  Calibration *cal_;  // struct containing calibration information
+  Calibration *cal_; // struct containing calibration information
   short sts_;        // return value from functions
   bool biasInit_ = false;
 
   std::array<float, 6> voltages_{
-      0, 0, 0, 0, 0, 0};  // array to store raw voltage measurements
-  std::array<float, 6> bias_{0, 0, 0, 0, 0, 0};  // array to store bias
+      0, 0, 0, 0, 0, 0}; // array to store raw voltage measurements
+  std::array<float, 6> bias_{0, 0, 0, 0, 0, 0}; // array to store bias
   std::array<float, 6> transformation_{
-      0, 0, 0, 0,
-      0, 0};  // transform includes a translation
-              // along the Z-axis and a rotation about the X-axis.
+      0, 0, 0, 0, 0, 0}; // transform includes a translation
+                         // along the Z-axis and a rotation about the X-axis.
 
   std::mutex voltage_mtx_;
 };
 
-#endif  // FTCONVERTER_H
+#endif // FTCONVERTER_H
