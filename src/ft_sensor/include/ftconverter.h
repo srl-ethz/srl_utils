03bea8b5c0b15dab7a3bfe4acca234fa06f7f61b
Tian Sizhe <yztszh168@163.com>
2023-03-07 11:29:08 +0100

    update init

--- src/ft_sensor/include/ftconverter.h
+++ src/ft_sensor/include/ftconverter.h
@@ -76,17 +76,14 @@ class FTConverter {
 
  private:
   // std::string calfilepath_;  // path to the calibration files
-//   Calibration *cal_;  // struct containing calibration information
   std::unique_ptr<Calibration> cal_; // pointer to struct containing calibration information
   short sts_;         // return value from functions
   bool biasInit_ = false;
 
-  std::array<float, 6> voltages_{
-      0, 0, 0, 0, 0, 0};  // array to store raw voltage measurements
-  std::array<float, 6> bias_{0, 0, 0, 0, 0, 0};  // array to store bias
-  std::array<float, 6> transformation_{
-      0, 0, 0, 0, 0, 0};  // transform includes a translation
-                          // along the Z-axis and a rotation about the X-axis.
+  std::array<float, 6> voltages_{};  // array to store raw voltage measurements
+  std::array<float, 6> bias_{};  // array to store bias
+  std::array<float, 6> transformation_{};  // transform includes a translation
+                                           // along the Z-axis and a rotation about the X-axis.
 
   std::mutex voltage_mtx_;
 };
