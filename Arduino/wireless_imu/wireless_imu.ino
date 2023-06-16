5c3d1c5c144d005cb32250e3beaed679e734f3d6
Yasunori Toshimitsu <yasu313nori@gmail.com>
2023-06-16 16:17:13 +0200

    faster serial, update and send only when deltat > 0

--- Arduino/wireless_imu/wireless_imu.ino
+++ Arduino/wireless_imu/wireless_imu.ino
@@ -1,6 +1,6 @@
 /*
   intended for Arduino nano 33 BLE board
-  read from IMU, do sensor fusion, and send quaternion over serial (TODO: over BLE)
+  read from IMU, do sensor fusion, and send quaternion (wxyz) over serial (TODO: over BLE)
   sensor fusion: https://github.com/aster94/SensorFusion
   reading from sensor: https://github.com/arduino-libraries/Arduino_LSM9DS1
 */
@@ -10,7 +10,7 @@
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial)
     ;
 
@@ -52,13 +52,18 @@ void loop()
   }
   deltat = fusion.deltatUpdate();
 
-  fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);
+  if (deltat > 0){
+    fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);
 
-  quatPtr = fusion.getQuat();
-  for (int i = 0; i < 4; i++)
-  {
-    Serial.print(quatPtr[i]);
-    Serial.print("\t");
+    quatPtr = fusion.getQuat();
+
+    // print with 4 decimal places
+    Serial.print(quatPtr[0], 4);
+    Serial.print(",");
+    Serial.print(quatPtr[1], 4);
+    Serial.print(",");
+    Serial.print(quatPtr[2], 4);
+    Serial.print(",");
+    Serial.println(quatPtr[3], 4);
   }
-  Serial.println();
 }
